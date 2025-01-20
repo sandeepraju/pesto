421b206830d1a894fe35dbc3c8f07be563e7679f
Sandeep Raju Prabhakar <me@sandeepraju.in>
2025-01-20 02:36:03 -0800

    Formatting fixes

--- src/app/projects/text-project.tsx
+++ src/app/projects/text-project.tsx
@@ -8,11 +8,13 @@ type TextProjectProps = {
 
 export default function TextProject({ title, description, url, bg = 'bg-slate-100' }: TextProjectProps) {
   return (
-    <div className={`display-block ${bg} rounded-lg shadow-lg max-w-full w-full m-2 p-6 text-center justify-center transition-transform duration-200 hover:-translate-y-1`}>
-      <a className="w-full h-full block" href={url}>
-        <h2 className={`text-2xl font-bold mb-4 text-gray-800`}>{title}</h2>
-        {description && <p className="text-gray-600">{description}</p>}
-      </a>
+    <div className="display-block max-w-full w-full p-2">
+      <div className={`display-block ${bg} rounded-lg shadow-lg max-w-full w-full p-6 text-center justify-center transition-transform duration-200 hover:-translate-y-1`}>
+        <a className="w-full h-full block" href={url}>
+          <h2 className={`text-2xl font-bold mb-4 text-gray-800`}>{title}</h2>
+          {description && <p className="text-gray-600">{description}</p>}
+        </a>
+      </div>
     </div>
   );
 }
