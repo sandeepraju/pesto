d6b0d13d1b02837522779c8f1ce2f7e2ff036b1d
Sandeep Raju Prabhakar <me@sandeepraju.in>
2025-01-20 02:36:57 -0800

    Update font, replace navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,27 +10,36 @@ import { LuFilePenLine, LuSquareUserRound } from "react-icons/lu";
 import { GoCodeSquare } from "react-icons/go";
 import Tilt from 'react-parallax-tilt';
 
+import { Merriweather } from 'next/font/google';
+
+const merriweather = Merriweather({
+  weight: ["700"],
+  subsets: ['latin'],
+  variable: '--font-merriweather-serif',
+});
+
 import Footer from "./footer";
 
 import config from '../data/config.json';
+import Nav from "./nav";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[4em_auto_4em] min-h-screen mx-auto gap-3">
+    <div className="grid grid-rows-[4em_auto_4em] min-h-screen mx-auto gap-3 md:max-w-screen-lg">
       <header className="flex items-center justify-center">
       </header>
       <main className="p-2">
         <Tilt className="block mx-auto h-auto">
           <Image
             className="rounded-full border-8 border-[#f8f9fa] shadow-xl block mx-auto h-auto"
-            src="/profile.jpg"
+            src="/img/profile.jpg"
             alt={config.name}
             width={300}
             height={300}
             priority
           />
         </Tilt>
-        <h1 className="text-3xl md:text-5xl font-bold text-center justify-center mx-auto pt-5">{config.name}</h1>
+        <h1 className={`text-3xl md:text-5xl font-bold text-center justify-center mx-auto pt-5 font-serif ${merriweather.variable}`}><strong>{config.name}</strong></h1>
         <p className="text-center mx-auto max-w-[32em] pt-5">
           {config.intro}
         </p>
@@ -81,44 +90,7 @@ export default function Home() {
             </a>
           </li>
         </ul>
-        <ul className="flex flex-col md:flex-row md:space-x-8 items-center justify-center mx-auto pt-5 text-lg">
-          <li className="p-2">
-            <a
-              className="inline-block transition-transform duration-200 hover:underline hover:underline-offset-4 decoration-dashed hover:-translate-y-1"
-              href="/about"
-              target="_self"
-              rel="noopener noreferrer">
-              <LuSquareUserRound className="inline-block scale-100 relative -top-[3px]" /> About
-            </a>
-          </li>
-          <li className="p-2">
-            <a
-              className="inline-block transition-transform duration-200 hover:underline hover:underline-offset-4 decoration-dashed hover:-translate-y-1"
-              href="/projects"
-              target="_self"
-              rel="noopener noreferrer">
-              <GoCodeSquare className="inline-block scale-100 relative -top-[3px]" /> Projects
-            </a>
-          </li>
-          <li className="p-2">
-            <a
-              className="inline-block transition-transform duration-200 hover:underline hover:underline-offset-4 decoration-dashed hover:-translate-y-1"
-              href="/Giovanni-Pestocchi-Resume.pdf"
-              target="_self"
-              rel="noopener noreferrer">
-              <GrDocumentPdf className="inline-block scale-100 relative -top-[3px]" /> Resume
-            </a>
-          </li>
-          <li className="p-2">
-            <a
-              className="inline-block transition-transform duration-200 hover:underline hover:underline-offset-4 decoration-dashed hover:-translate-y-1"
-              href="/projects"
-              target="_self"
-              rel="noopener noreferrer">
-              <LuFilePenLine className="inline-block scale-100 relative -top-[3px]" /> Blog
-            </a>
-          </li>
-        </ul>
+        <Nav />
       </main>
       <Footer />
     </div>
